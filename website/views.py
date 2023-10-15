58e72fdc48109a8d4112ccc044da91b1000cdcf9
mavulag <mavulageofrey1@gmail.com>
2023-10-16 00:38:44 +0300

    Modifying dcrm/urls.py, website/urls.py, website/views.py, and creating base.html, home.html and navbar.html

--- website/views.py
+++ website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    # Check to see if logging in
+    # if the user is posting
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+    # Else if user not posting
+    else:
+        return render(request, 'home.html', {})
+
+# naming login_user so as to reduce the conflict with the imported library
+# def login_user(request):
+#     pass
+
+# naming login_user so as to reduce the conflict with the imported library
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('home')
