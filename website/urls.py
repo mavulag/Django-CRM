f90a5c5721bdb3cf82ec2dcb64999a65307f5c5f
mavulag <mavulageofrey1@gmail.com>
2023-10-16 00:42:00 +0300

    Adding register url in website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # You will use this for the independent login page
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
